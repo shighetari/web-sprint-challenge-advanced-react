9b087d3b8f7a9ba76bb84862e0f5b0c7f6f8045d
Francisco Barrios <shighetari@gmail.com>
2020-06-05 14:09:29 -0400

    First commit

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,22 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("rendering the checkout form", () => {
+    render(<CheckoutForm />)
 
-test("form shows success message on submit with form details", () => {});
+});
+
+test("form header renders", () => {
+
+    render(<CheckoutForm />)
+    const header = screen.getByText(/checkout form/i)
+    
+});
+
+test("form shows success message on submit with form details", () => {
+
+
+});
